e814f091dd7d840e5653c21953e982b5512a8de4
Fikri A.N <itachiuciha189@gmail.com>
2024-06-07 22:32:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/andro_cilent/ApiService.java
@@ -0,0 +1,17 @@
+package com.example.andro_cilent;
+
+import com.example.andro_cilent.User;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @POST("insert_user.php")
+    Call<Void> insertUser(@Body User user);
+
+    @GET("get_users.php")
+    Call<List<User>> getUsers();
+}
